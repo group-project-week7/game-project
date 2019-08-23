3e0038f5dea75a7b7395ddd92b5f2d4b7e5c2175
nicholasbilly <nicholasbilly@gmail.com>
2019-08-23 13:45:13 +0700

    add store.js

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,9 +4,70 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div>
+      <div>
+          <div v-for="(room, i) in rooms" :key="i">
+              {{room.title}}
+              <button @click="join(room.id)"> join room </button>
+              <button @click="leave(room.id)"> leave room </button>
+          </div>
+          <button @click="roomCreate">create Room</button>
+      </div>
+      <div>
+        <form @submit.prevent="userCreate">
+        <input type="text" v-model="username">
+        <input type="submit" value="create User"> 
+        </form>
+      </div>
+  </div>
   </div>
 </template>
+
+<script>
+import Room from './components/Room'
+import store from './store'
+import { mapState } from 'vuex'
+import db from './api/firebase'
+
+
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data() {
+    return{
+      username: ''
+    }  
+  },
+  computed: mapState([
+    'rooms',
+  ]),
+  methods: {
+    roomCreate() {
+      this.$store.dispatch('createRoom')
+    },
+
+    userCreate() {
+      this.$store.dispatch('createUser', this.username)
+      this.username = ''
+    },
+
+    join(id) {
+      this.$store.dispatch('joinRoom', id)
+    },
+
+    leave(id) {
+      this.$store.dispatch('leaveRoom', id)
+    }
+  },
+  created() {
+    this.$store.dispatch('getRooms')
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ client/src/components/Room.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'room',
+     
+}
+</script>
+    
+<style>
+
+</style>
