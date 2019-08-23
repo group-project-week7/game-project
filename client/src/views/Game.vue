acdec6950f43fc432681283443cfc92a94bebe51
iFaujack <fauzi.nurfadillahh@gmail.com>
2019-08-23 11:51:09 +0700

    adding slap effect

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid" :style="boardStyle" @click="gantiCursor">
     <div style="display: flex; height: 100vh; width: 100vw; flex-direction: column;">
-      <h3 class="pt-5 pb-5">TIME : {{timer}}</h3>
-      <div class="middle-content" :style="normalCursor">
-        <img
-          src="../assets/rocco.gif"
-          class="mosquito"
-          :key="i"
-          @click.prevent="kill(i)"
-          v-for="(mosquito,i) in mosquitos"
-          v-if="mosquito.status"
-          :style="mosquito.style"
-        />
+      <div  class="middle-content" @click.prevent="slapEffect">
+          <div style="top: 20px; left: 30px; position: absolute;">
+            <h3> Time : {{timer}} </h3>
+            <h3> Score : {{score}} </h3>
+          </div>
+          <img
+            src="../assets/rocco.gif"
+            class="mosquito"
+            :key="i"
+            @click.prevent="kill(i)"
+            v-for="(mosquito,i) in mosquitos"
+            v-if="mosquito.status"
+            draggable="false"
+            :style="mosquito.style"
+          />
+        </div>
       </div>
-    </div>
   </div>
 </template>
 
@@ -28,8 +32,10 @@ export default {
       imgHeight: 64,
       imgWidth: 64,
       changeInterval: 1000,
-      timer : 10,
-      normalCursor: "cursor:url('../assets/swatter.png'), auto"
+      timer : 30,
+      boardStyle : {
+        cursor : `url(${require('../assets/swatter.png')}),auto`,
+      },
     };
   },
   methods: {
@@ -37,18 +43,22 @@ export default {
       for (let i=0; i<this.mosquitos.length; i++){
         let randompx = Math.floor(Math.random() * 1000)
         var x = Math.floor(Math.random() * randompx);
-        var y = Math.floor(Math.random() * randompx);
-        var tempX = Number(this.mosquitos[i].style.top.split('px')[0]) + x
-        var tempY = Number(this.mosquitos[i].style.left.split('px')[0]) + y
+        var y = Math.floor(Math.random() * window.innerWidth);
         this.mosquitos[i].style.top = `${x}px`;
         this.mosquitos[i].style.left = `${y}px`;
       }
-      
+    },
+    gantiCursor(){
+      this.boardStyle.cursor = `url(${require('../assets/swatter-hit.png')}),auto`
     },
     kill(index) {
+      this.slapEffect()
       this.mosquitos[index].status = false;
       this.score += 10;
-     
+    },
+    slapEffect(){
+        var audio = new Audio(require('../assets/slap.mp3'))
+        audio.play()
     },
     randomPosition() {
       const randomPos = twoSizes => Math.round(Math.random() * twoSizes);
@@ -91,8 +101,8 @@ export default {
 .middle-content {
   justify-content: center;
   align-content: center;
-  width: 80%;
-  height: 70vh;
+  width: 100%;
+  height: 100vh;
   background: wheat;
   margin: auto;
   position: relative;
@@ -100,13 +110,12 @@ export default {
   background-image: url('../assets/bg_game.png');
   background-size: cover;
   background-position: 0 0;
-  /* ;
-  cursor:url('../assets/swatter-hit.png'),; */
+  cursor: url("../assets/swatter.png"),auto;
 }
 .mosquito {
   position: absolute;
   top: 0px;
   left: 0px;
-  transition: all 5s;
+  transition: all 3s;
 }
 </style>
